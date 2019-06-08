dd1d7135f30f7dddd625e71a03d3396fcf9e4e84
Nguyễn Trung Thắng <trungthangdk@gmail.com>
2019-06-08 09:35:47 +0700

    add template

--- src/main/java/com/main/home/controller/IndexController.java
+++ src/main/java/com/main/home/controller/IndexController.java
@@ -1,5 +1,14 @@
 package com.main.home.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+	@GetMapping(value = "/")
+	public String home(Model model) {
+		return "home/index";
+	}
 
 }
