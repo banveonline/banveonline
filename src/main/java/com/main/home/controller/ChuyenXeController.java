ce45894911beeb8404358939d2b66f5915509b46
saobacdau96 <39641521+saobacdau96@users.noreply.github.com>
2019-06-13 12:41:38 +0700

    chuyến xe

--- src/main/java/com/main/home/controller/ChuyenXeController.java
+++ src/main/java/com/main/home/controller/ChuyenXeController.java
@@ -1,5 +1,7 @@
 package com.main.home.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,13 +16,52 @@ public class ChuyenXeController {
 	@Autowired
 	private ChuyenXeDAO chuyenXeDAO;
 
-	@GetMapping(value = "/chuyenxe2/{comment}")
+	@GetMapping(value = "/chuyenxe/{comment}")
 	public String index(@PathVariable String comment, ModelMap modelMap) {
-		if(comment.equals("all")) {
+		if (comment.equals("all")) {
 			modelMap.put("chuyenxe", chuyenXeDAO.findAll());
-		}else if(comment.equals("hcm-hn")) {
-			modelMap.put("chuyenxe", chuyenXeDAO.timChuyenXeCungTuyen("Hồ Chí Minh", "Hà Nội"));
+		} else {
+			modelMap.put("chuyenxe",
+					chuyenXeDAO.timChuyenXeCungTuyen(spitComment(comment).get(0), spitComment(comment).get(1)));
 		}
-		return "home/garage";
+		return "home/trips";
+	}
+
+	public ArrayList<String> spitComment(String comment) {
+		ArrayList<String> ls = new ArrayList<>();
+		String noiDi = null;
+		String noiDen = null;
+		String[] c = comment.split("-");
+		switch (c[0]) {
+		case "hcm":
+			noiDi = "Hồ Chí Minh";
+			break;
+		case "hn":
+			noiDi = "Hà Nội";
+			break;
+		case "dn":
+			noiDi = "Đà Nẵng";
+			break;
+		case "nt":
+			noiDi = "Nha Trang";
+			break;
+		}
+		switch (c[1]) {
+		case "hcm":
+			noiDen = "Hồ Chí Minh";
+			break;
+		case "hn":
+			noiDi = "Hà Nội";
+			break;
+		case "dn":
+			noiDi = "Đà Nẵng";
+			break;
+		case "nt":
+			noiDi = "Nha Trang";
+			break;
+		}
+		ls.add(noiDi);
+		ls.add(noiDen);
+		return ls;
 	}
 }
--- src/main/java/com/main/home/controller/NhaXeController.java
+++ src/main/java/com/main/home/controller/NhaXeController.java
@@ -1,21 +1,16 @@
 package com.main.home.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import com.main.admin.DAO.NhaXeDAO;
 
 @Controller
 public class NhaXeController {
 
-	@Autowired
-	private NhaXeDAO nhaXeDAO;
 	
-	@GetMapping(value = "/chuyenxe/{comment}")
-	public String index(@PathVariable String comment,ModelMap modelMap ) {
+	@GetMapping(value = "/nhaxe")
+	public String index(ModelMap modelMap ) {
 		
 		return "home/garage";
 	}
